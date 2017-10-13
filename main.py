dc6a794a8911a0a331b34984b7751bd6e5c52475
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-13 01:19:54 -0700

    Moved Population object from main.py to simulation.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
 # main.py (infectorSimulation)
 
 import object
-
-class Population(object):
-
-    def __init__(self, group_size):
-        self.group_size = group_size
-        pass
-
--- simulation.py
+++ simulation.py
@@ -1,3 +1,9 @@
 # simulation.py (infectorSimulation)
 
 # import main
+
+class Population(object):
+
+    def __init__(self, group_size):
+        self.group_size = group_size
+        pass
