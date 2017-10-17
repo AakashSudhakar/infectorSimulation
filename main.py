2d48e460f78b6d65b1228e8a3827ca2d2bf560e1
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-17 12:32:41 -0700

    Creating simulation start functions

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 # main.py (infectorSimulation)
 
 import object
+
+# Function that takes no inputs from command-line but starts game
+def simulation_start(self):
+    self.play_counter = 0
+
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("\n\n\nWelcome to the Infector Simulation.\n\n")
+    
+    if self.play_counter < 1:
+        # Simply select a virus and continue through simulation
+        play_game()
+    elif:
+        # Ask user if they want to run simulation again with same virus or pick a new virus
+        play_game()
+
+# Function that accepts simulation_start parameters and plays the game while initializing all user inputs
+def simulation_play(self):
+    virus_choice = ""
+    pass
--- simulation.py
+++ simulation.py
@@ -2,8 +2,5 @@
 
 # import main
 
-class Population(object):
-
-    def __init__(self, group_size):
-        self.group_size = group_size
-        pass
+if __name__ == "__main__":
+    pass
