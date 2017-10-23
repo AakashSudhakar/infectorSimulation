807865deaf7d8e5f13620577f5650006a776d1f9
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-23 03:49:16 -0700

    Restructuring simulation

--- simulation.py
+++ simulation.py
@@ -1,10 +1,14 @@
 # simulation.py (infectorSimulation)
 
-# import main
+import random, sys
+random.seed(42)
+from person import Person
+from logger import Logger
+from virus import Virus
 
 class Simulation(object):
 
-    def __init__(self):
+    def __init__(self, total_population, persons_vaccinated, virus_type, mortality_rate, reproduction_rate, initial_infected=1):
         pass
 
     def test01(self):
