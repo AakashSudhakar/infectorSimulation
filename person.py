08d06aa71867e0746073a7c7da1cbad97a6644e5
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-23 03:47:38 -0700

    Polished Person class

--- person.py
+++ person.py
@@ -1,3 +1,25 @@
 # subject.py (infectorSimulation)
 
 # import main
+
+class Person(object):
+
+    def __init__(self, _id, is_vaccinated, infected=None):
+        self._id = _id
+        self.is_vaccinated = is_vaccinated
+        self.infected = infected
+        self.is_alive = True
+
+    
+    def infection_occurrence(self, infection_id):
+        self.infected = infection_id
+
+
+    def person_survives_infection(self):
+        if not self.is_vaccinated:
+            rand = random()
+
+            if rand < self.infected.mortality_rate:
+                self.is_alive = False
+                return False
+        return True
